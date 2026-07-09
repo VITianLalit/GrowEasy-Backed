d4b1a329bebecb26b42a70919cf1b3fb643b9ec5
VITianLalit <lalitsahuji786@gmail.com>
2026-07-09 13:43:02 +0530

    fixed

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,3 +1,14 @@
 import app from '../src/app';
+import { IncomingMessage, ServerResponse } from 'http';
 
-export default app;
+// Disable Vercel's automatic body parsing so that multer can handle
+// multipart/form-data (file uploads) correctly.
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default function handler(req: IncomingMessage, res: ServerResponse) {
+  return app(req, res);
+}
